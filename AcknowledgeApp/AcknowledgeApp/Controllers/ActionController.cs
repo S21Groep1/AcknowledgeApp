d95e3d169ec4a5131fef6af964b1485a48a02e97
JariScholten <jari.scholten@gmail.com>
2018-04-04 11:54:36 +0200

    first changes

--- AcknowledgeApp/AcknowledgeApp/Controllers/ActionController.cs
+++ AcknowledgeApp/AcknowledgeApp/Controllers/ActionController.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AcknowledgeApp.Models;
 
 namespace AcknowledgeApp.Controllers
 {
-    public class HomeController : Controller
+    public class ActionController : Controller
     {
         public IActionResult Index()
         {
