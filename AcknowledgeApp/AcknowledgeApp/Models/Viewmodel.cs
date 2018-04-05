69641e691fabe50b391b194df3bfee66be9d21b1
twanbeeren <twanbeeren@users.noreply.github.com>
2018-04-05 11:11:06 +0200

    Actionpoints work

--- AcknowledgeApp/AcknowledgeApp/Models/Viewmodel.cs
+++ AcknowledgeApp/AcknowledgeApp/Models/Viewmodel.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Models;
 
 namespace AcknowledgeApp.Models
 {
     public class Viewmodel
     {
+        Actionpoint actiepunt = new Actionpoint();
+        
+        
+        public List<string> Actionpoints
+        {
+            get
+            {
+                return actiepunt.Softskilllist(); ;
+            }
+        }
+
 
     }
 }
